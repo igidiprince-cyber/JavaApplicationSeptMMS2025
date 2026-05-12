faf6836d5488b2c05d718d8d543429fd30912bc0
igidiprince-cyber <igidiprince@gmail.com>
2026-05-12 13:35:22 +0100

    Chapter2 solu 14

---
+++ SquaresCubes.java
@@ -0,0 +1,12 @@
+// Squares and Cubes
+
+public class SquaresCubes {
+    public static void main(String[] args) {
+
+        System.out.println("number\tsquare\tcube");
+
+        for (int i = 0; i <= 10; i++) {
+            System.out.printf("%d\t%d\t%d%n", i, (i * i), (i * i * i));
+        }
+    }
+}

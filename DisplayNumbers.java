9f12e7a19fa0814053682c13edf5636f9ebb9a7f
igidiprince-cyber <igidiprince@gmail.com>
2026-05-12 11:05:38 +0100

    Chapter2 soln 3

---
+++ DisplayNumbers.java
@@ -0,0 +1,18 @@
+// Display Numbers 1 to 4
+
+public class DisplayNumbers {
+    public static void main(String[] args) {
+
+        // a
+        System.out.println("1 2 3 4");
+
+        // b
+        System.out.print("1 ");
+        System.out.print("2 ");
+        System.out.print("3 ");
+        System.out.print("4\n");
+
+        // c
+        System.out.printf("1 2 3 4%n");
+    }
+}

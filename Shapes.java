3b0a628868e4ca34043d7bae4abd4741b20ad143
igidiprince-cyber <igidiprince@gmail.com>
2026-05-12 11:21:37 +0100

    Chapter2 soln 6

---
+++ Shapes.java
@@ -0,0 +1,16 @@
+// Shapes with Asterisks
+
+public class Shapes {
+    public static void main(String[] args) {
+
+        System.out.println("*********     ***       *       *");
+        System.out.println("*       *   *     *    ***     * *");
+        System.out.println("*       *  *       *  *****   *   *");
+        System.out.println("*       *  *       *    *    *     *");
+        System.out.println("*       *  *       *    *   *       *");
+        System.out.println("*       *  *       *    *    *     *");
+        System.out.println("*       *  *       *    *     *   *");
+        System.out.println("*       *   *     *     *      * *");
+        System.out.println("*********     ***       *       *");
+    }
+}

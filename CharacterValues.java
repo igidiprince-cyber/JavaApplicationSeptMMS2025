caf556dd0d7c71e13f0e16af0d48450b7923c996
igidiprince-cyber <igidiprince@gmail.com>
2026-05-12 11:53:49 +0100

    Chapter2 soln 12

---
+++ CharacterValues.java
@@ -0,0 +1,25 @@
+// Integer Values of Characters
+
+public class CharacterValues {
+    public static void main(String[] args) {
+
+        System.out.printf("A = %d%n", (int)'A');
+        System.out.printf("B = %d%n", (int)'B');
+        System.out.printf("C = %d%n", (int)'C');
+
+        System.out.printf("a = %d%n", (int)'a');
+        System.out.printf("b = %d%n", (int)'b');
+        System.out.printf("c = %d%n", (int)'c');
+
+        System.out.printf("0 = %d%n", (int)'0');
+        System.out.printf("1 = %d%n", (int)'1');
+        System.out.printf("2 = %d%n", (int)'2');
+
+        System.out.printf("$ = %d%n", (int)'$');
+        System.out.printf("* = %d%n", (int)'*');
+        System.out.printf("+ = %d%n", (int)'+');
+        System.out.printf("/ = %d%n", (int)'/');
+
+        System.out.printf("Space = %d%n", (int)' ');
+    }
+}

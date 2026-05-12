e0cc1604d24d490bc6a0becc8a770548d0785d8d
igidiprince-cyber <igidiprince@gmail.com>
2026-05-12 11:36:24 +0100

    Chapter2 soln 10

---
+++ Checkerboard.java
@@ -0,0 +1,15 @@
+// Checkerboard Pattern
+
+public class Checkerboard {
+    public static void main(String[] args) {
+
+        System.out.println("* * * * * * * *");
+        System.out.println(" * * * * * * * *");
+        System.out.println("* * * * * * * *");
+        System.out.println(" * * * * * * * *");
+        System.out.println("* * * * * * * *");
+        System.out.println(" * * * * * * * *");
+        System.out.println("* * * * * * * *");
+        System.out.println(" * * * * * * * *");
+    }
+}
